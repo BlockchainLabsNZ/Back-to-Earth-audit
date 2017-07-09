6e4cfa6be809b7929aa96a9a4d399523a0eda2c5
Isaac <isaac@3months.com>
2017-07-10 09:27:01 +1200

    Copy sample tests in

---
+++ contracts/ContractReceiver.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.11;
+
+ /*
+ * Contract that is working with ERC223 tokens
+ */
+
+contract ContractReceiver {
+  function tokenFallback(address _from, uint _value, bytes _data) {
+  }
+}
